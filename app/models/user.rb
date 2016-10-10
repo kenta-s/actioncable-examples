7713d3d1eae8b3cd1a1691268dbb83f089699da7
kenta-s <knt01222@gmail.com>
2016-10-10 17:50:27 +0900

    Add necessary methods to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,19 @@
 class User < ActiveRecord::Base
   has_many :messages
   has_many :comments
+
+  def appear(on: nil)
+    # do something
+    true
+  end
+
+  def disappear
+    # do something
+    true
+  end
+
+  def away
+    # do something
+    true
+  end
 end
